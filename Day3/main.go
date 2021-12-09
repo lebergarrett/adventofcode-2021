5f9668d36fcb876382d189d644953ef829206dfd
lebergarrett <lebergarrett@gmail.com>
2021-12-09 04:02:21 +0000

    Day 3 part 1 cleanup

--- Day3/main.go
+++ Day3/main.go
@@ -10,56 +10,22 @@ import (
 func main() {
 	binarynumbers := readInput("inputfile.txt")
 
-	// Calculate number of zeros and ones in each position
-	numzeros := make([]int, len(binarynumbers[0]))
-	numones := make([]int, len(binarynumbers[0]))
-	for _, numstr := range binarynumbers {
-		for i, digit := range numstr {
-			// convert ascii to num
-			digit -= 48
-			if digit == 0 {
-				numzeros[i] += 1
-			} else {
-				numones[i] += 1
-			}
-		}
-	}
+	numzeros, numones := zerosAndOnes(binarynumbers)
 	fmt.Println("Num Zeros:", numzeros)
 	fmt.Println("Num Ones: ", numones)
 
-	// Build up gamma and epsilon
-	gamma := make([]int, len(binarynumbers[0]))
-	epsilon := make([]int, len(binarynumbers[0]))
-	for i := range numzeros {
-		if numzeros[i] > numones[i] {
-			gamma[i] = 0
-			epsilon[i] = 1
-		} else {
-			gamma[i] = 1
-			epsilon[i] = 0
-		}
-	}
+	gamma, epsilon := calcGammaAndEpsilon(numzeros, numones)
 	fmt.Println("Gamma:    ", gamma)
 	fmt.Println("Epsilon:  ", epsilon)
 
-	// turn slices of bits into decimal(base10) value
-	var decgamma float64
-	// creates 2 iterators, one that counts down and one that counts up
-	for i, j := len(gamma)-1, 0; i >= 0; i, j = i-1, j+1 {
-		decgamma += float64(gamma[i]) * math.Pow(2, float64(j))
-	}
+	decgamma := convertToBase10(gamma)
 	fmt.Println("decgamma: ", decgamma)
-
-	var decepsilon float64
-	for i, j := len(epsilon)-1, 0; i >= 0; i, j = i-1, j+1 {
-		decepsilon += float64(epsilon[i]) * math.Pow(2, float64(j))
-	}
+	decepsilon := convertToBase10(epsilon)
 	fmt.Println("decepsilon: ", decepsilon)
 
 	// Calculate and output power consumption
 	powerconsumption := decgamma * decepsilon
-	fmt.Print("What is the power consumption of the submarine? ")
-	fmt.Printf("%f\n", powerconsumption)
+	fmt.Printf("What is the power consumption of the submarine? %f\n", powerconsumption)
 }
 
 // Opening the inputfile and transposing it to a slice
@@ -80,4 +46,46 @@ func readInput(inputfile string) []string {
 
 	file.Close()
 	return output
+}
+
+// Calculate number of zeros and ones in each position
+func zerosAndOnes(slice []string) (numzeros []int, numones []int) {
+	numzeros = make([]int, len(slice[0]))
+	numones = make([]int, len(slice[0]))
+
+	for _, numstr := range slice {
+		for i, digit := range numstr {
+			// convert ascii to num
+			digit -= 48
+			if digit == 0 {
+				numzeros[i] += 1
+			} else {
+				numones[i] += 1
+			}
+		}
+	}
+
+	return numzeros, numones
+}
+
+func calcGammaAndEpsilon(numzeros []int, numones []int) (gamma []int, epsilon []int) {
+	gamma = make([]int, len(numzeros))
+	epsilon = make([]int, len(numzeros))
+	for i := range numzeros {
+		if numzeros[i] > numones[i] {
+			gamma[i], epsilon[i] = 0, 1
+		} else {
+			gamma[i], epsilon[i] = 1, 0
+		}
+	}
+	return gamma, epsilon
+}
+
+// turn slices of bits into decimal(base10) value
+func convertToBase10(slice []int) (dec float64) {
+	// creates 2 iterators, one that counts down and one that counts up
+	for i, j := len(slice)-1, 0; i >= 0; i, j = i-1, j+1 {
+		dec += float64(slice[i]) * math.Pow(2, float64(j))
+	}
+	return dec
 }
