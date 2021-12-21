b8d4dc0159cdb7bf4f893d0595880f6f01224384
Garrett Leber <lebergarrett@gmail.com>
2021-12-20 18:02:47 -0700

    Cleaning up main func

--- Day2/Part1/main.go
+++ Day2/Part1/main.go
@@ -6,50 +6,52 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
-	"github.com/fatih/color"
 )
 
 func main() {
-	// Opening the inputfile and transposing it to a slice
-	inputfile, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
-	}
-
-	scanner := bufio.NewScanner(inputfile)
-	scanner.Split(bufio.ScanLines)
-	var commands []string
-
-	for scanner.Scan() {
-		commands = append(commands, scanner.Text())
-	}
-
-	inputfile.Close()
+	commands := readInput("input.txt")
 
 	// Create vars for storing coords
-	var x int
-	var y int
+	var pos int
+	var depth int
 	// Main logic loop
 	for _, command := range commands {
 		words := strings.Fields(command)
 		direction := words[0]
 		value, _ := strconv.Atoi(words[1])
 
-		if direction == "forward" {
-			x += value
-			fmt.Print(command)
-			color.New(color.FgGreen).Add(color.Bold).Print(" (Increased X Coord ", x, ")\n")
-		} else if direction == "up" {
-			y -= value
-			fmt.Print(command)
-			color.New(color.FgRed).Add(color.Bold).Print(" (Decreased Y Coord ", x, ")\n")
-		} else {
-			y += value
-			fmt.Print(command)
-			color.New(color.FgYellow).Add(color.Bold).Print(" (Increased Y Coord ", x, ")\n")
-		}
+		pos, depth = movement(direction, value, pos, depth)
 	}
 	fmt.Println("What do you get if you multiply your final horizontal position by your final depth?", x*y)
+}
+
+// opening the inputfile and transposing it to a slice
+func readInput(inputfile string) []string {
+	file, err := os.Open(inputfile)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var output []string
+	for scanner.Scan() {
+		output = append(output, scanner.Text())
+	}
+
+	file.Close()
+	return output
+}
+
+func movement(direction string, value int, pos int, depth int) (int, int) {
+	if direction == "forward" {
+		pos += value
+	} else if direction == "up" {
+		depth -= value
+	} else {
+		depth += value
+	}
+	return pos, depth
 }
